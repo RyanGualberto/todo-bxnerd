78715b70436a938b90fe09578444f2182ac72586
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-11-25 19:52:12 -0300

    trying css

--- todo-server/server.js
+++ todo-server/server.js
@@ -2,21 +2,23 @@ const express = require("express");
 const server = express();
 const swaggerUi = require("swagger-ui-express");
 const cors = require("cors");
+const ROOT_FOLDER = path.join(__dirname, "..");
 
 const routes = require("./src/routes/index.routes");
 const swaggerDocument = require("./src/docs/swagger_output.json");
+const options = {
+  customCssUrl: "/swagger-ui.css",
+  customSiteTitle: "ToDo API",
+};
 
 const PORT = 3000;
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(cors());
+server.use(express.static(path.join(ROOT_FOLDER, "public")));
 
 server.use("/api/v1", routes);
-const options = {
-  customCssUrl: "/swagger-ui.css",
-  customSiteTitle: "ToDo API",
-};
 
 server.use(
   "/api/v1/docs",
